4893766db962f478458ca4db517497b07328efa7
Redm4x <max_maxime_max@hotmail.com>
2021-04-21 17:40:31 -0400

    Add deployment list loaded from static file

--- api/server.js
+++ api/server.js
@@ -2,6 +2,9 @@
 
 const express = require("express");
 const path = require("path");
+const deployments = require("./data/deployments.json");
+
+const nodeApi = "http://135.181.60.250:1317/";
 
 // Constants
 const PORT = 3080;
@@ -24,6 +27,32 @@ app.get("/api/users", (req, res) => {
   // console.log("/api/users called!", req);
   res.json(users);
 });
+
+
+app.get('/api/getDeployments/', async function(req, res) {
+  const data = deployments.deployments.map(x => (
+    {
+      owner: x.deployment.deployment_id.owner,
+      dseq: x.deployment.deployment_id.dseq,
+      state: x.deployment.state,
+      price: x.groups.flatMap(g => g.group_spec.resources.map(r => r.price)).map(p => parseInt(p.amount)).reduce((a,b) => a + b),
+      groups: x.groups.map(g => ({
+        gseq: g.group_id.gseq,
+        state: g.state,
+        name: g.group_spec.name,
+        resources: g.group_spec.resources.map(r => ({
+          cpuUnits: r.resources.cpu.units.val,
+          storageUnits: r.resources.storage.quantity.val,
+          memoryUnits: r.resources.memory.quantity.val,
+          count: r.count,
+          price: parseInt(r.price.amount)
+        }))
+      }))
+    }
+  ));
+  res.send(data);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on the port::${PORT}`);
