4a6f85f7996d055feabb6b95589603e0e6741c45
Redm4x <max_maxime_max@hotmail.com>
2021-04-21 20:42:45 -0400

    Move address link and deployment list into their own files

---
+++ app/src/AddressLink.js
@@ -0,0 +1,3 @@
+export default function AddressLink(props) {
+    return <a href={"https://akash.bigdipper.live/account/" + props.address} target="_blank" rel="noopener noreferrer">{props.address}</a>
+}
--- app/src/App.js
+++ app/src/App.js
@@ -1,80 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React, { useEffect, useState } from "react";
-import { useTable, useFilters, useExpanded } from 'react-table'
 
-import SelectColumnFilter from "./SelectColumnFilter";
-import DeploymentDetailRow from "./DeploymentDetailRow";
-
-function AddressLink(props) {
-  return <a href={"https://akash.bigdipper.live/account/" + props.address} target="_blank" rel="noopener noreferrer">{props.address}</a>
-}
+import DeploymentList from "./DeploymentList";
 
 function App() {
   const [users, setUsers] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [deployments, setDeployments] = useState([]);
-
-  const columns = React.useMemo(() => [
-    {
-      id: 'expander',
-      Cell: ({ row }) =>
-        <span className="expandCell"
-          {...row.getToggleRowExpandedProps()}
-        >
-          {row.isExpanded ? <i className="bi bi-dash"></i> : <i className="bi bi-plus"></i>}
-        </span>
-    },
-    {
-      Header: 'Owner',
-      Cell: ({ value }) => <AddressLink address={value} />,
-      accessor: 'owner',
-      disableFilters: true
-    },
-    {
-      Header: 'DSEQ',
-      accessor: 'dseq',
-      disableFilters: true
-    },
-    {
-      Header: 'State',
-      Filter: SelectColumnFilter,
-      accessor: 'state'
-    },
-    {
-      Header: "Price",
-      Cell: ({ value }) => <>{value} uakt</>,
-      accessor: "price",
-      disableFilters: true
-    }
-  ], []);
-
-  const tableInstance = useTable({
-    columns,
-    data: deployments,
-    defaultCanFilter: false
-  }, useFilters, useExpanded);
-
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-    visibleColumns
-  } = tableInstance;
-
-  useEffect(async () => {
-    getDeployments();
-  }, []);
-
-  async function getDeployments() {
-    setIsLoading(true);
-    const response = await fetch("/api/getDeployments");
-    const body = await response.json();
-    setIsLoading(false);
-    setDeployments(body);
-  }
 
   // get the users
   useEffect(() => {
@@ -102,53 +33,8 @@ function App() {
         )}
 
         <br />
-
-        <div className="container">
-          {deployments.length > 0 && (
-            <table className="table table-dark caption-top" {...getTableProps()}>
-              <caption>Deployments</caption>
-              <thead>
-                {headerGroups.map(headerGroup => (
-                  <tr {...headerGroup.getHeaderGroupProps()}>
-                    {
-                      headerGroup.headers.map(column => (
-                        <th {...column.getHeaderProps()}>{console.log(column)}
-                          {column.render('Header')}
-                          <div>{column.canFilter ? column.render('Filter') : null}</div>
-                        </th>
-                      ))}
-                  </tr>
-                ))}
-              </thead>
-              <tbody {...getTableBodyProps()}>
-                {
-                  rows.map(row => {
-                    prepareRow(row);
-                    return (
-                      <React.Fragment {...row.getRowProps()}>
-                        <tr className="tableRow">
-                          {row.cells.map(cell => {
-                            return (
-                              <td {...cell.getCellProps()}>
-                                {cell.render('Cell')}
-                              </td>
-                            )
-                          })}
-                        </tr>
-                        {row.isExpanded ? (
-                          <tr>
-                            <td colSpan={visibleColumns.length}>
-                              <DeploymentDetailRow deployment={row.original} />
-                            </td>
-                          </tr>
-                        ) : null}
-                      </React.Fragment>
-                    )
-                  })}
-              </tbody>
-            </table>
-          )}
-        </div>
+        
+        <DeploymentList />
       </header>
     </div>
   );
