c3a7746ccf41c499f99ff54380dbb8a6aec9d6dd
Redm4x <max_maxime_max@hotmail.com>
2021-05-16 02:39:20 -0400

    Handle create deployment

--- app/src/components/DeployTool/DeployTool.js
+++ app/src/components/DeployTool/DeployTool.js
@@ -6,11 +6,18 @@ import { DirectSecp256k1HdWallet, Registry, Writer, Coin, Tx } from "@cosmjs/pro
 import { AminoMsgSend, AminoMsgDelegate } from "@cosmjs/amino";
 import { Any } from "@cosmjs/stargate/build/codec/google/protobuf/any";
 import protobuf from "protobufjs/minimal";
-import { MsgCloseDeployment, MsgRevokeCertificate, MsgCreateCertificate } from "./ProtoAkashTypes";
+import { MsgCloseDeployment, MsgRevokeCertificate, MsgCreateCertificate, MsgCreateDeployment } from "./ProtoAkashTypes";
+
+import DemoDeployYaml from "../../demo.deploy.yml";
 
 import rsa from 'js-x509-utils';
 var rs = require("jsrsasign");
+const yaml = require('js-yaml');
 
+const rpcEnpoint = "http://localhost:4242/http://rpc.akash.forbole.com";
+const apiEndpoint = "http://localhost:4242/http://135.181.60.250:1317";
+// const rpcEnpoint = "http://localhost:26656";
+// const apiEndpoint = "http://localhost:1317";
 
 // import * as protoDeployment from '../../proto/gen/akash/deployment/v1beta1/deployment_pb';
 // console.log(protoDeployment);
@@ -192,13 +199,19 @@ export function DeployTool() {
     }, []);
 
     async function loadValidCertificates(owner) {
-
-        const apiEndpoint = "http://localhost:1317/http://135.181.60.250:1317";
-
         const response = await fetch(apiEndpoint + "/akash/cert/v1beta1/certificates/list?filter.state=valid&filter.owner=" + owner);
         const data = await response.json();
 
         setValidCertificates(data.certificates);
+    }
+
+    // TODO: Verify
+    async function getCurrentHeight() {
+        const response = await fetch(apiEndpoint + "/blocks/latest");
+        const data = await response.json();
+
+        const height = parseInt(data.block.header.height); // TODO parseInt?
+        return height;
     }
 
     async function closeDeployment(deployment) {
@@ -266,19 +279,399 @@ export function DeployTool() {
     }
 
     async function createDeployment() {
+        const fromAddress = selectedWallet.bech32Address;
 
+        const flags = {};
+        const response = await fetch(DemoDeployYaml);
+        const txt = await response.text();
+        const doc = yaml.load(txt);
+
+        const dd = await NewDeploymentData(doc, flags, fromAddress); // TODO Flags
+
+        const msg = {
+            id: dd.deploymentId,
+            groups: dd.groups,
+            version: dd.version,
+            deposit: dd.deposit
+        };
+
+        const txData = {
+            "typeUrl": "/akash.deployment.v1beta1.MsgCreateDeployment",
+            "value": msg
+        }
+
+        console.log(txData);
+
+        debugger;
+        const err = MsgCreateDeployment.verify(msg);
+        const encoded = MsgCreateDeployment.fromObject(msg);
+        const decoded = MsgCreateDeployment.toObject(encoded);
+        console.log(decoded);
+
+        if(err) throw err;
+
+        const offlineSigner = window.getOfflineSigner(chainId);
+
+        const customRegistry = new Registry();
+        customRegistry.register("/akash.deployment.v1beta1.MsgCreateDeployment", MsgCreateDeployment);
+
+        const client = await SigningStargateClient.connectWithSigner(rpcEnpoint, offlineSigner, {
+            registry: customRegistry
+        });
+
+        await client.signAndBroadcast(fromAddress, [txData], fee);
+    }
+
+    function ParseServiceProtocol(input) {
+        let result;
+
+        // This is not a case sensitive parse, so make all input
+        // uppercase
+        if (input) {
+            input = input.toUpperCase();
+        }
+
+        switch (input) {
+            case "TCP", "", undefined: // The empty string (no input) implies TCP
+                result = "TCP";
+                break;
+            case "UDP":
+                result = "UDP";
+                break;
+            default:
+                throw "ErrUnsupportedServiceProtocol";
+        }
+
+        return result;
+    }
+
+    function parseSizeStr(str) {
+        const suffixPos = str.indexOf("Mi");// Handle other suffix
+        const numberStr = str.substring(0, suffixPos);
+        const result = parseInt(numberStr) * 1024 * 1024;
+        return result.toString();
+    }
+
+    // Port of: func (sdl *v2ComputeResources) toResourceUnits() types.ResourceUnits
+    function toResourceUnits(computeResources) {
+        if (!computeResources) return {};
+debugger;
+        let units = {};
+        if (computeResources.cpu) {
+            units.cpu = {
+                units: { val: (computeResources.cpu.units * 1000).toString() },
+                //attributes: computeResources.cpu.attributes TODO
+            }
+        }
+        if (computeResources.memory) {
+            units.memory = {
+                quantity: { val: parseSizeStr(computeResources.memory.size) },
+                //attributes: computeResources.memory.attributes TODO
+            }
+        }
+        if (computeResources.storage) {
+            units.storage = {
+                quantity: { val: parseSizeStr(computeResources.storage.size) },
+                //attributes: computeResources.storage.attributes TODO
+            }
+        }
+
+        return units;
+    }
+
+    // Port of:    func (sdl *v2) Manifest() (manifest.Manifest, error
+    function Manifest(yamlJson) {
+        let groups = {};
+
+        Object.keys(yamlJson.services).forEach(svcName => {
+            const svc = yamlJson.services[svcName];
+            const depl = yamlJson.deployment[svcName];
+
+            Object.keys(depl).forEach(placementName => {
+                const svcdepl = depl[placementName];
+                let group = groups[placementName];
+
+                if (!group) {
+                    group = {
+                        name: placementName,
+                        services: []
+                    };
+                    groups[placementName] = group;
+                }
+
+                const compute = yamlJson.profiles.compute[svcdepl.profile];
+
+                const msvc = {
+                    name: svcName,
+                    image: svc.image,
+                    args: svc.args,
+                    env: svc.env,
+                    resources: toResourceUnits(compute.resources),
+                    count: svcdepl.count,
+                    expose: []
+                };
+
+                svc.expose.forEach((expose) => {
+                    const proto = ParseServiceProtocol(expose.proto);
+
+                    if (expose.to && expose.to.length > 0) {
+                        expose.to.forEach(to => {
+                            msvc.expose.push({
+                                service: to.service,
+                                port: expose.port,
+                                externalPort: expose.as,
+                                proto: proto,
+                                global: to.global,
+                                hosts: expose.accept && expose.accept.items
+                            });
+                        })
+                    } else {
+                        msvc.expose.push({
+                            service: "",
+                            port: expose.port,
+                            externalPort: expose.as,
+                            proto: proto,
+                            global: false,
+                            hosts: expose.accept.items
+                        });
+                    }
+                });
+
+                msvc.expose = msvc.expose.sort((a, b) => {
+                    if (a.service != b.service) {
+                        return a.service < b.service;
+                    }
+                    if (a.port != b.port) {
+                        return a.port < b.port;
+                    }
+                    if (a.proto != b.proto) {
+                        return a.proto < b.proto;
+                    }
+                    if (a.global != b.global) {
+                        return a.global < b.global;
+                    }
+                    return false;
+                });
+
+                group.services.push(msvc);
+            })
+        });
+
+        let names = Object.keys(groups);
+        names = names.sort((a, b) => a < b);
+
+        let result = names.map(name => groups[name]);
+        return result;
+    }
+
+    function shouldBeIngress(expose) {
+        return expose.proto == "TCP" && expose.global && 80 == exposeExternalPort(expose);
+    }
+
+    function exposeExternalPort(expose) {
+        if (expose.externalPort === 0) {
+            return expose.port;
+        }
+
+        return expose.externalPort;
+    }
+
+    function sortKeys(obj_1) {
+        var key = Object.keys(obj_1)
+            .sort(function order(key1, key2) {
+                if (key1 < key2) return -1;
+                else if (key1 > key2) return +1;
+                else return 0;
+            });
+
+        // Taking the object in 'temp' object
+        // and deleting the original object.
+        var temp = {};
+
+        for (var i = 0; i < key.length; i++) {
+            temp[key[i]] = obj_1[key[i]];
+            delete obj_1[key[i]];
+        }
+
+        // Copying the object from 'temp' to 
+        // 'original object'.
+        for (var i = 0; i < key.length; i++) {
+            obj_1[key[i]] = temp[key[i]];
+        }
+        return obj_1;
+    }
+
+    function DeploymentGroups(yamlJson) {
+        let groups = {};
+
+        Object.keys(yamlJson.services).forEach(svcName => {
+            const svc = yamlJson.services[svcName];
+            const depl = yamlJson.deployment[svcName];
+
+            Object.keys(depl).forEach(placementName => {
+                const svcdepl = depl[placementName];
+                const compute = yamlJson.profiles.compute[svcdepl.profile];
+                const infra = yamlJson.profiles.placement[placementName];
+                const price = infra.pricing[svcdepl.profile];
+                price.amount = price.amount.toString(); // Interpreted as number otherwise
+
+                let group = groups[placementName];
+                
+                if (!group) {
+                    group = {
+                        name: placementName,
+                        requirements: {
+                            attributes: Object.keys(infra.attributes).map(key => ({ key: key, value: infra.attributes[key] })),
+                            signed_by: {
+                                all_of: infra.signedBy.allOf,
+                                any_of: infra.signedBy.anyOf
+                            }
+                        },
+                        resources: []
+                    };
+
+                    group.requirements.attributes = group.requirements.attributes.sort((a, b) => a.key < b.key);
+
+                    groups[group.name] = group;
+                }
+
+
+                const resources = {
+                    resources: toResourceUnits(compute.resources), // Chanded resources => unit
+                    price: price,
+                    count: svcdepl.count
+                };
+
+                let endpoints = [];
+
+                svc.expose.forEach((expose) => {
+                    expose.to.forEach(to => {
+                        if (to.global) {
+                            const proto = ParseServiceProtocol(expose.proto);
+
+                            let v = {
+                                port: expose.port,
+                                externalPort: expose.as,
+                                proto: proto,
+                                service: to.service,
+                                global: to.global,
+                                hosts: expose.accept?.items
+                            };
+
+                            // TODO Enum
+                            const Endpoint_SHARED_HTTP = 0;
+                            const Endpoint_RANDOM_PORT = 1;
+
+                            let kind = Endpoint_RANDOM_PORT;
+
+                            if (shouldBeIngress(v)) {
+                                kind = Endpoint_SHARED_HTTP;
+                            }
+
+                            endpoints.push({ kind: kind });
+                        }
+                    });
+
+                    resources.resources.endpoints = endpoints;
+                    group.resources.push(resources);
+                });
+            });
+        });
+
+        let names = Object.keys(groups);
+        names = names.sort((a, b) => a < b);
+
+        let result = names.map(name => groups[name]);
+        return result;
+    }
+
+    // https://github.com/cosmos/cosmos-sdk/blob/9fd866e3820b3510010ae172b682d71594cd8c14/types/utils.go#L29
+    function SortJSON(jsonStr) {
+        return JSON.stringify(JSON.parse(jsonStr));
+    }
+
+    function _arrayBufferToBase64(buffer) {
+        var binary = '';
+        var bytes = new Uint8Array(buffer);
+        var len = bytes.byteLength;
+        for (var i = 0; i < len; i++) {
+            binary += String.fromCharCode(bytes[i]);
+        }
+        return window.btoa(binary);
+    }
+
+    // https://github.com/ovrclk/akash/blob/04e7a7667dd94b5a15fa039e4f7df5c9ad93be4f/sdl/sdl.go#L120
+    async function ManifestVersion(manifest) {
+        var enc = new TextEncoder();
+        let m = JSON.stringify(manifest);
+        let sortedBytes = enc.encode(SortJSON(m));
+
+        let sum = await crypto.subtle.digest('SHA-256', sortedBytes);
+
+        let base64 = _arrayBufferToBase64(sum);
+
+        return base64;
+    }
+
+    // https://github.com/ovrclk/akash/blob/04e7a7667dd94b5a15fa039e4f7df5c9ad93be4f/x/deployment/client/cli/flags.go#L51
+    function DeploymentIDFromFlagsForOwner(flags, owner) {
+        let id = {
+            owner: owner,
+        };
+
+        //TODO
+        // if id.DSeq, err = flags.GetUint64("dseq"); err != nil {
+        //     return id, err
+        // }
+
+        return id;
+    }
+
+
+    function DepositFromFlags(flags) {
+        // let val = flags["deposit"];
+
+        // if(!val) return {};
+
+        // return ParseCoinNormalized(val)
+        // TODO
+        return {
+            "denom": "uakt",
+            "amount": "5000000"
+        };
+    }
+
+    async function NewDeploymentData(yamlJson, flags, fromAddress) {
+        const groups = DeploymentGroups(yamlJson);
+        const mani = Manifest(yamlJson);
+        const ver = await ManifestVersion(mani);
+        const id = DeploymentIDFromFlagsForOwner(flags, fromAddress); // TODO: handle flags
+        const deposit = DepositFromFlags();
+
+        if (!id.dseq) {
+            id.dseq = (await getCurrentHeight());
+        }
+
+        return {
+            sdl: yamlJson,
+            manifest: mani,
+            groups: groups,
+            deploymentId: id,
+            orderId: [],
+            leaseId: [],
+            version: ver,
+            deposit: deposit
+        };
     }
 
     async function revokeCertificate(cert) {
         const offlineSigner = window.getOfflineSigner(chainId);
 
-        const endpoint = "http://localhost:1317/http://rpc.akash.forbole.com";
-
         const customRegistry = new Registry();
         customRegistry.register("/akash.cert.v1beta1.MsgRevokeCertificate", MsgRevokeCertificate);
 
 
-        const client = await SigningStargateClient.connectWithSigner(endpoint, offlineSigner, {
+        const client = await SigningStargateClient.connectWithSigner(rpcEnpoint, offlineSigner, {
             registry: customRegistry
         });
 
@@ -292,10 +685,7 @@ export function DeployTool() {
             }
         };
 
-
-
         await client.signAndBroadcast(selectedWallet.bech32Address, [revokeCertificateJson], fee, "Test Akashlytics");
-
     }
 
     async function createCertificate() {
@@ -304,8 +694,6 @@ export function DeployTool() {
         const notBefore = new Date();
         let notAfter = new Date();
         notAfter.setFullYear(notBefore.getFullYear() + 1);
-        
-
 
         // STEP1. generate a key pair
         var kp = rs.KEYUTIL.generateKeypair("EC", "secp256r1");
@@ -319,9 +707,6 @@ export function DeployTool() {
             version: 3,
             serial: { int: Math.floor((new Date()).getTime() * 1000) },
             issuer: { str: "/CN=" + fromAddress },
-            
-//   notbefore: "201231235959Z",
-//   notafter:  "221231235959Z",
             notbefore: notBefore,
             notafter: notAfter,
             subject: { str: "/CN=" + fromAddress },
@@ -344,47 +729,7 @@ export function DeployTool() {
         // STEP3. show PEM strings of keys and a certificate
         console.log(prvpem);
         console.log(pubpem);
-        console.log(crtpem); // certificate object is signed automatically with "cakey" value.
-
-        debugger;
-        // var keys = window.forge.pki.rsa.generateKeyPair(2048);
-        // var cert = window.forge.pki.createCertificate();
-        // cert.publicKey = keys.publicKey;
-        // cert.serialNumber = Math.floor((new Date()).getTime() * 1000).toString();
-        // cert.validity.notBefore = new Date();
-        // cert.validity.notAfter = new Date();
-        // cert.validity.notAfter.setFullYear(cert.validity.notBefore.getFullYear() + 1);
-
-        // var attrs = [{
-        //     name: 'commonName',
-        //     value: fromAddress
-        // }];
-        // cert.setSubject(attrs);
-        // cert.setIssuer(attrs);
-
-        // cert.setExtensions([
-        //     {
-        //         name: 'keyUsage',
-        //         keyEncipherment: true,
-        //         dataEncipherment: true
-        //     },
-        //     {
-        //         name: 'extKeyUsage',
-        //         clientAuth: true
-        //     },
-        //     {
-        //         name: 'basicConstraints',
-        //         cA: true
-        //     }
-        // ]);
-
-        // cert.sign(keys.privateKey, window.forge.md.sha256.create());
-        // var privatePem = window.forge.pki.privateKeyToPem(keys.privateKey);
-        // var pem = window.forge.pki.certificateToPem(cert);
-        // var publicPem = window.forge.pki.publicKeyToPem(keys.publicKey);
-        // localStorage.setItem("DeploymentCertificatePrivateKey", prvpem);
-        // localStorage.setItem("DeploymentCertificatePublicKey", pubpem);
-        // localStorage.setItem("DeploymentCertificate", crtpem);
+        console.log(crtpem);
 
         const createCertificateMsg = {
             "typeUrl": "/akash.cert.v1beta1.MsgCreateCertificate",
@@ -397,12 +742,10 @@ export function DeployTool() {
 
         const offlineSigner = window.getOfflineSigner(chainId);
 
-        const endpoint = "http://localhost:1317/http://rpc.akash.forbole.com";
-
         const customRegistry = new Registry();
         customRegistry.register("/akash.cert.v1beta1.MsgCreateCertificate", MsgCreateCertificate);
 
-        const client = await SigningStargateClient.connectWithSigner(endpoint, offlineSigner, {
+        const client = await SigningStargateClient.connectWithSigner(rpcEnpoint, offlineSigner, {
             registry: customRegistry
         });
 
--- app/src/components/DeployTool/ProtoAkashTypes.js
+++ app/src/components/DeployTool/ProtoAkashTypes.js
@@ -1,4 +1,4 @@
-import { Field, Type } from "protobufjs";
+import { Field, Enum, Type } from "protobufjs";
 
 // Deployments
 
@@ -14,50 +14,79 @@ const Coin = new Type("Coin")
     .add(new Field("denom", 1, "string"))
     .add(new Field("amount", 2, "string"));
 
+const Kind = new Enum("Kind", { "SHARED_HTTP": 0, "RANDOM_PORT": 1 });
+
 const Endpoint = new Type("Endpoint")
-    .add("kind", 1, "enum");
+    .add(new Field("kind", 1, "Kind"))
+    .add(Kind);
 
 const Attribute = new Type("Attribute")
-    .add("key", 1, "string")
-    .add("value", 2, "string");
+    .add(new Field("key", 1, "string"))
+    .add(new Field("value", 2, "string"));
 
 const ResourceValue = new Type("ResourceValue")
-    .add("val", 1, "bytes");
+    .add(new Field("val", 1, "string"));
 
 const CPU = new Type("CPU")
-    .add("units", 1, "ResourceValue")
-    .add("attributes", 2, "Array.Attribute");
+    .add(new Field("units", 1, "ResourceValue"))
+    .add(ResourceValue)
+    .add(new Field("attributes", 2, "Attribute", "repeated"))
+    .add(Attribute);
 
 const Memory = new Type("Memory")
-    .add("quantity", 1, "ResourceValue")
-    .add("attributes", 2, "Array.Attribute");
+    .add(new Field("quantity", 1, "ResourceValue"))
+    .add(ResourceValue)
+    .add(new Field("attributes", 2, "Attribute", "repeated"))
+    .add(Attribute);
 
 const Storage = new Type("Storage")
-    .add("quantity", 1, "ResourceValue")
-    .add("attributes", 2, "Array.Attribute");
+    .add(new Field("quantity", 1, "ResourceValue"))
+    .add(ResourceValue)
+    .add(new Field("attributes", 2, "Attribute", "repeated"))
+    .add(Attribute);
 
 const ResourceUnits = new Type("ResourceUnits")
     .add(new Field("cpu", 1, "CPU"))
+    .add(CPU)
     .add(new Field("memory", 2, "Memory"))
+    .add(Memory)
     .add(new Field("storage", 3, "Storage"))
-    .add(new Field("enpoints", 4, "Array.Endpoint"));
+    .add(Storage)
+    .add(new Field("endpoints", 4, "Endpoint", "repeated"))
+    .add(Endpoint);
 
 const Resource = new Type("Resource")
-    .add(new Field("resources", 1, "ResourceUnits"))
+    .add(new Field("resources", 1, "ResourceUnits"))// unit
+    .add(ResourceUnits)
     .add(new Field("count", 2, "uint32"))
-    .add(new Field("price", 3, "Coin"));
+    .add(new Field("price", 3, "Coin"))
+    .add(Coin);
+
+const SignedBy = new Type("SignedBy")
+    .add(new Field("all_of", 1, "string", "repeated"))
+    .add(new Field("any_of", 2, "string", "repeated"));
+
+const PlacementRequirements = new Type("PlacementRequirements")
+    .add(new Field("signed_by", 1, "SignedBy"))
+    .add(SignedBy)
+    .add(new Field("attributes", 2, "Attribute", "repeated"))
+    .add(Attribute);
 
 const GroupSpec = new Type("GroupSpec")
     .add(new Field("name", 1, "string"))
     .add(new Field("requirements", 2, "PlacementRequirements"))
-    .add(new Field("resources", 3, "Array.Resource"));
+    .add(PlacementRequirements)
+    .add(new Field("resources", 3, "Resource", "repeated"))
+    .add(Resource);
 
 export const MsgCreateDeployment = new Type("MsgCreateDeployment")
     .add(new Field("id", 1, "DeploymentID"))
     .add(DeploymentID)
-    .add(new Field("groups", 2, "GroupSpec"))
+    .add(new Field("groups", 2, "GroupSpec", "repeated"))
+    .add(GroupSpec)
     .add(new Field("version", 3, "bytes"))
-    .add(new Field("deposit", 4, "Coin"));
+    .add(new Field("deposit", 4, "Coin"))
+    .add(Coin);
 
 // Certificates
 
